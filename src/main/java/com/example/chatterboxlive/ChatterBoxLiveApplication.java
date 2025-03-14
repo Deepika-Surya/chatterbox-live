0f50261d162df265274848f36c62b6fbeb301de1
Deepika S <deepikasurya947@gmail.com>
2025-03-14 09:48:21 +0530

    ChatterBoxLiveApplication.java

---
+++ src/main/java/com/example/chatterboxlive/ChatterBoxLiveApplication.java
@@ -0,0 +1,11 @@
+package com.example.chatterboxlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterBoxLiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatterBoxLiveApplication.class, args);
+    }
+}
